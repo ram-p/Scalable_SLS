df6f30fef05bc70cfd0330743c5de86b8cc5d2cf
Ram Padmanabhan <rampadmanabhan0609@gmail.com>
2026-01-20 16:52:15 -0600

    Complete state-feedback
    To be verified; yet to simulate.

--- L1.m
+++ L1.m
@@ -1,4 +1,7 @@
-function [x, u, K_final, t_fault, N] = L1(ctr, T)
+close all
+clear
+clc
+format compact
 
 % Function that takes in a parameter ctr for which controller to use, and
 % returns state and input history.
@@ -7,6 +10,7 @@ function [x, u, K_final, t_fault, N] = L1(ctr, T)
 % ctr = 3; memoryless
 
 M = 2;      % Number of systems
+T = 10;     % Time horizon
 
 % System dynamics
 % Continuous-time
@@ -29,7 +33,7 @@ wbar = 1;
 vbar = 1;
 
 % Setting up subwords. This is specific to the case of only one switch.
-k = 2;                  % Subword length (memory over switching signal)
+k = 4;                  % Subword length (memory over switching signal)
 lengthP = k+1;          % Number of possible subwords (M^k in the most general case)
 P = cell(1, lengthP);   % Initializing set of all possible subwords
 % lengthP is thus the number of possible controllers after subword-based
@@ -40,122 +44,77 @@ for i = 1:lengthP
     P{i} = [ones(1, lengthP-i) M*ones(1, i-1)];
 end
 
-% Count for prefix constraints - compare number of ones with last signal
-for i = 1:lengthP
-    ct(i) = nnz(P{i} == P{end});
-end
-
-%% Everything below this is incomplete, and must be modified for subword clustering.
 % We now use cvx to code the optimization using SLS.
+% This is the state-feedback case. Working on output-feedback as well.
 
 cvx_begin quiet
-variable Phixx(n*(T+1), n*(T+1), lengthL) lower_triangular;
-variable Phixy(n*(T+1), m*(T+1), lengthL) lower_triangular;
-variable Phiux(p*(T+1), n*(T+1), lengthL) lower_triangular;
-variable Phiuy(p*(T+1), m*(T+1), lengthL) lower_triangular;
+% variable Phixx(n*(T+1), n*(T+1), lengthP) lower_triangular;
+% variable Phixy(n*(T+1), m*(T+1), lengthP) lower_triangular;
+% variable Phiux(p*(T+1), n*(T+1), lengthP) lower_triangular;
+% variable Phiuy(p*(T+1), m*(T+1), lengthP) lower_triangular;
+variable Phix(n*(T+1), n*(T+1), lengthP)
+variable Phiu(p*(T+1), n*(T+1), lengthP)
 variable gam
 
-for i = 1:lengthL
-    N(i) = norm([Phixx(:,:,i) Phixy(:,:,i)], Inf);
+% Setting up objective: is this correct under subword memory?
+for i = 1:lengthP
+    N(i) = norm(Phix(:,:,i), Inf);
 end
 
-% for i = 1:size(Phixx, 1)
-%     for j = 1:size(Phixx,2)
-%         if abs(j-i) >= n
-%             Phixx(i,j,:) == 0;
-%         end
-%     end
-% end
-%
-% for i = 1:size(Phixy, 1)
-%     for j = 1:size(Phixy,2)
-%         if i-j >= n || j-i >= m
-%             Phixy(i,j,:) == 0;
-%         end
-%     end
-% end
-%
-% for i = 1:size(Phiux, 1)
-%     for j = 1:size(Phiux,2)
-%         if i-j >= p || j-i >= n
-%             Phiux(i,j,:) == 0;
-%         end
-%     end
-% end
-%
-% for i = 1:size(Phiuy, 1)
-%     for j = 1:size(Phiuy,2)
-%         if i-j >= p || j-i >= m
-%             Phiuy(i,j,:) == 0;
-%         end
-%     end
-% end
+% Setting up next subwords
+P_next = cell(1, lengthP);      % Set of possible next subwords
+P_next{1} = {1, 2};   % If p = 11, p+ = 11 or 12.
+for i = 2:lengthP-1
+    P_next{i} = {i+1};     % Intermediate subwords are just shifted.
+end
+P_next{end} = {lengthP};       % If p = 22, p+ = 22.
 
 minimize gam
 gam >= max(N)
 
-for i = 1:lengthL
-    % Affine constraints from SLS, also block diagonality.
-    [eye(n*(T+1))-Z*Ablock{i} -Z*Bblock{i}]*[Phixx(:,:,i) Phixy(:,:,i); Phiux(:,:,i) Phiuy(:,:,i)] ...
-        == [eye(n*(T+1)) zeros(n*(T+1),m*(T+1))]
-    [Phixx(:,:,i) Phixy(:,:,i); Phiux(:,:,i) Phiuy(:,:,i)]*[eye(n*(T+1))-Z*Ablock{i}; -Cblock{i}] ...
-        == [eye(n*(T+1)); zeros(p*(T+1),n*(T+1))]
-
-    % Prefix constraints due to switching
-    % All are equated to the last Phi^sigma since that is how the count
-    % is set up.
-    Phixx(1:ct(i)*n, 1:ct(i)*n, i) == Phixx(1:ct(i)*n, 1:ct(i)*n, end)
-    Phixy(1:ct(i)*n, 1:ct(i)*m, i) == Phixy(1:ct(i)*n, 1:ct(i)*m, end)
-    Phiux(1:ct(i)*p, 1:ct(i)*n, i) == Phiux(1:ct(i)*p, 1:ct(i)*n, end)
-    Phiuy(1:ct(i)*p, 1:ct(i)*m, i) == Phiuy(1:ct(i)*p, 1:ct(i)*m, end)
-
-    % Polytope containment constraints from Farkas' lemma
-    % Lambda(:,:,i)*HE ...
-    %     == HS*[Phixx(n+1:n*(T+1),:,i) Phixy(n+1:n*(T+1),:,i); Phiux(1:p*T,:,i) Phiuy(1:p*T,:,i)]
-    % % Lambda(:,:,i)*hE <= hS
-    % sum(Lambda(:,:,i), 2) <= 1
+% Identity constraint
+for t = 1:T-1
+    Phix(((t+1)*n+1):(t+2)*n, (t*n+1):(t+1)*n, :) = eye(n);
 end
+
+% Affine constraints from SLS imposed iteratively over subwords.
+for tau = 0:T
+for t = tau:T-1
+    for i = 1:lengthP
+    for j = P_next{i}
+        j = j{:};
+        Phix(((t+1)*n+1):(t+2)*n, (tau*n+1):(tau+1)*n, j) == A{P{i}(end)}*Phix((t*n+1):(t+1)*n, (tau*n+1):(tau+1)*n, i) ...
+          + B{P{i}(end)}*Phiu((t*p+1):(t+1)*p, (tau*p+1):(tau+1)*p, i)
+    end
+    end
+end
+end
+
 cvx_end
 
-K = cell(1, lengthL);
-for i = 1:lengthL
-    K{i} = Phiuy(:,:,i) - Phiux(:,:,i)*(Phixx(:,:,i)\Phixy(:,:,i));
+K = cell(1, lengthP);
+for i = 1:lengthP
+    K{i} = Phiu(:,:,i)/Phix(:,:,i);
 end
 
+% Below this is incomplete. Simulation to be modified.
 x0 = -1+2*rand(n,1);
 x = [x0 zeros(n,T)];
 u = zeros(p,T);
-y = zeros(m*(T+1),1);
+% y = zeros(m*(T+1),1);
 
 [~,tf] = max(N);
 t_fault = tf+1;         % Fault time step (in Matlab array index)
-L_fault = L{t_fault};   % Faulty switching signal
-
-% Note that for convenience in code, we can define K_FTC ahead of time due
-% to prefix constraints.
-K_FTC = K{t_fault};             % Fault tolerant controller for that signal
-K_NF = K{end};                  % Non-fault tolerant controller
-% Controller with no memory
-K_diag = zeros(size(K_FTC));
-for i = 1:size(K_FTC,1)
-    for j = 1:size(K_FTC,2)
-        if i == j
-            K_diag(i,j) = K_NF(i,j);
-        end
-    end
-end
-
-K_final = (ctr==1)*K_FTC + (ctr==2)*K_NF + (ctr==3)*K_diag;
+sigma = [ones(1, tf) M*ones(1, T-tf+1)];
 
 for t = 1:T
     w = 2*randi(2,n,1)-3;
     v = 2*randi(2,m,1)-3;
-    % w = 2*rand(n,1)-1;
-    % v = 2*rand(m,1)-1;
 
-    y((m*(t-1)+1):m*t) = C{L_fault(t)}*x(:,t) + v;
-    u(:,t) = K_final(p*(t-1)+1:p*t,:)*y;
+    sig = [ones(1,k-t) sigma(t-k+1:t)];             % Last k letters of switching signal
+    K_final = K{cellfun(@(x) isequal(x,sig), P)};   % Use controller corresponding to last k letters
+
+    % y((m*(t-1)+1):m*t) = C{L_fault(t)}*x(:,t) + v;
+    u(:,t) = K_final(p*(t-1)+1:p*t,:)*vec(x);
     x(:,t+1) = A{L_fault(t)}*x(:,t) + B{L_fault(t)}*u(:,t) + w;
-end
-
 end
